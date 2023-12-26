ed60eea94ffc4b0fa1a3df59dd90b54d9f9b27f5
Recepgrgl <115144327+Recepgrgl@users.noreply.github.com>
2023-12-26 09:49:45 +0300

    OgretmenBL.cs

--- OkulApp.BLL/OgretmenBL.cs
+++ OkulApp.BLL/OgretmenBL.cs
@@ -6,20 +6,20 @@ using System.Runtime.CompilerServices;
 
 namespace OkulApp.BLL//Bussiness Logic Layer
 {
-    public class OgrenciBL
+    public class OgretmenBL
     {
-        public bool OgrenciEkle(Ogretmen ogr)
+        public bool OgretmenEkle(Ogretmen ogretmen)
         {
             try
             {
                 SqlParameter[] p = {
-                                  new SqlParameter("@Ad",ogr.Ad),
-                                  new SqlParameter("@Soyad",ogr.Soyad),
-                                  new SqlParameter("@Numara",ogr.Numara)
+                                  new SqlParameter("@Ad",ogretmen.Ad),
+                                  new SqlParameter("@Soyad",ogretmen.Soyad),
+                                  new SqlParameter("@Tc",ogretmen.Tc)
                             };
 
                 var hlp = new Helper();
-                return hlp.ExecuteNonQuery("Insert into tblOgrenciler (Ad,Soyad,Numara) values (@Ad,@Soyad,@Numara)", p) > 0;
+                return hlp.ExecuteNonQuery("Insert into tblOgretmenler (Ad,Soyad,Tc) values (@Ad,@Soyad,@Tc)", p) > 0;
             }
             catch (SqlException)
             {
